0a67b0397342cdba367ac739c9c67d9d243a3399
MgSaiNyiNyiTun <56770099+mgsainyinyitun@users.noreply.github.com>
2020-11-17 23:08:37 +0630

    add functionalities

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+import React from 'react';
+
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '937f33b864884f96812aff26f314b51f'
+ });
 
 const particleOptions = {
         particles: {
@@ -31,7 +40,39 @@ const particleOptions = {
           
         },
       }
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      input :'',
+      imageUrl:'',
+    }
+  }
+
+  onInputChange= (event)=>{
+    this.setState({
+      input:event.target.value
+    })
+  }
+  onSearchSubmit= ()=>{
+    this.setState({
+      imageUrl:this.state.input
+    })
+    app.models.predict(
+      "c0c0ac362b03416da06ab3fa36fb58e3",
+      this.state.input
+    ).then(
+      function(response){
+        console.log(response.outputs[0].data.regions);
+      },
+      function(err){
+        //error code
+      }
+    );
+  }
+
+
+  render(){
   return (
     <div className="App">
       <Particles
@@ -41,8 +82,15 @@ function App() {
       <Navigation/>
       <Rank/>
       <Logo/>
-      <ImageLinkForm/>
+      <ImageLinkForm 
+        onInputChange = {this.onInputChange}
+        onSearchSubmit = {this.onSearchSubmit}
+      />
+      <FaceRecognition
+        imageUrl={this.state.imageUrl}
+      />
     </div>
   );
+  }
 }
 export default App;
